ef89e87f01781528afbaf8654904b1135fb74d3e
Guy Owen <guy.owen@dpc.vic.gov.au>
2022-05-06 11:36:18 +1000

    [DDS-1182] Migrated API Client updates for env vars plugin.

--- api/plugins/action/env_variable.py
+++ api/plugins/action/env_variable.py
@@ -26,21 +26,20 @@ class ActionModule(ActionBase):
 
         display.v("Task args: %s" % self._task.args)
 
-        breakpoint()
-
-        lagoon = ApiClient(
-            task_vars.get('lagoon_api_endpoint'),
-            task_vars.get('lagoon_api_token'),
-            task_vars.get('options')
-        )
-
+        name = self._task.args.get('name')
         type = self._task.args.get('type')
         type_name = self._task.args.get('type_name')
-        name = self._task.args.get('name')
         state = self._task.args.get('state', 'present')
         value = self._task.args.get('value', None)
         scope = self._task.args.get('scope', None)
         replace_existing = self._task.args.get('replace_existing', False)
+        options = self._task.args.get('options', {'headers': {}})
+
+        lagoon = ApiClient(
+            task_vars.get('lagoon_api_endpoint'),
+            task_vars.get('lagoon_api_token'),
+            options
+        )
 
         # Setting this option will ensure the value has been set, by making
         # additional calls to the API until it matches.
