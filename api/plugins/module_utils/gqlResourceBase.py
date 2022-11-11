e0e2947d4b10e4ecab76280edf3022aba5475ddb
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2022-11-11 17:43:42 +1100

    Added a Project class for common queries.
    - Added the ability to specify fields.
    - Made sure to only fetch one level of fields each time to avoid API timeouts.
    - Also added ability to populate partial data - when the server times out, data is also returned with the errors. We might want to build on that in the future.

---
+++ api/plugins/module_utils/gqlResourceBase.py
@@ -0,0 +1,18 @@
+import re
+
+from ansible_collections.lagoon.api.plugins.module_utils.gql import GqlClient
+from ansible.utils.display import Display
+
+display = Display()
+
+class ResourceBase:
+
+    def __init__(self, client: GqlClient, options: dict = {}) -> None:
+        self.client = client
+        self.errors = []
+        self.display = display
+
+        self.batch_size = options.get('batch_size', 100)
+
+    def sanitiseForQueryAlias(self, name):
+        return re.sub(r'[\W-]+', '_', name)
