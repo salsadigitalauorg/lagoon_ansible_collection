83dd17ce8078854b71e30098e1294dbd2e90b25a
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2022-11-15 18:34:11 +1100

    Added class for variable add & delete.
    - Added role tasks to set default lagoon variables.
    - Added playbooks to test new plugins.

--- api/plugins/action/variables.py
+++ api/plugins/action/variables.py
@@ -3,8 +3,9 @@ __metaclass__ = type
 
 from ansible.plugins.action import ActionBase
 from ansible.utils.display import Display
-from ansible_collections.lagoon.api.plugins.module_utils.api_client import ApiClient
-from ansible.module_utils._text import to_native
+from ansible_collections.lagoon.api.plugins.module_utils.gql import GqlClient
+from ansible_collections.lagoon.api.plugins.module_utils.gqlEnvironment import Environment
+from ansible_collections.lagoon.api.plugins.module_utils.gqlProject import Project
 from ansible.errors import AnsibleError
 
 display = Display()
@@ -28,17 +29,28 @@ class ActionModule(ActionBase):
         if not 'headers' in options:
             options['headers'] = {}
 
-        lagoon = ApiClient(
-            task_vars.get('lagoon_api_endpoint'),
-            task_vars.get('lagoon_api_token'),
-            options
+        lagoon = GqlClient(
+            self._templar.template(task_vars.get('lagoon_api_endpoint')),
+            self._templar.template(task_vars.get('lagoon_api_token')),
+            self._task.args.get('headers', {})
         )
 
         if type == "project":
-            result['data'] = lagoon.project_get_variables(name)
+            lagoonProject = Project(lagoon).byName(name, ['id', 'name'])
+            if not len(lagoonProject.projects):
+                raise AnsibleError("Project not found.")
+
+            lagoonProject.withVariables()
+            result['data'] = lagoonProject.projects[0]['envVariables']
 
         elif type == "environment":
-            result['data'] = lagoon.environment_get_variables(name)
+            lagoonEnvironment = Environment(lagoon).byNs(
+                name, ['id', 'kubernetesNamespaceName'])
+            if not len(lagoonEnvironment.environments):
+                raise AnsibleError("Environment not found.")
+
+            lagoonEnvironment.withVariables()
+            result['data'] = lagoonEnvironment.environments[0]['envVariables']
 
         else:
             raise AnsibleError("Invalid 'type' provided.")
