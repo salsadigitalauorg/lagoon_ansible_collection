822921fac7073c730184e3d29089e1505f10de04
rashed-k <rashedkhan.e36@gmail.com>
2024-02-14 15:35:34 +1100

    test

--- api/plugins/action/deploy_target_config.py
+++ api/plugins/action/deploy_target_config.py
@@ -45,6 +45,13 @@ class ActionModule(LagoonActionBase):
                             self._display.vvvv(f"deleting config {config}")
                             DeployTargetConfig(self.client).delete(
                                 project['id'], config['_existing_id'])
+                            #Added deploytargetconfig cleanup 
+                            for existing_config in project["deployTargetConfigs"]:
+                                if existing_config['id'] in existing_config_ids and existing_config['branches'] not in specified_branch_patterns:
+                                    self._display.vvvv(f"Deleting unmatched config with ID {existing_config['id']}")
+                                    if DeployTargetConfig(self.client).delete(project['id'], existing_config['id']):
+                                        result['result'].append({'id': existing_config['id'], 'deleted': True})
+                                        result['changed'] = True
 
                         self._display.vvvv(f"adding config {config}")
                         addResult = DeployTargetConfig(self.client).add(
@@ -59,14 +66,8 @@ class ActionModule(LagoonActionBase):
                         else:
                             config['failed'] = True
                         result['result'].append(config)
-                        result['changed'] = True
-                        #Added deploytargetconfig cleanup 
-                        for existing_config in project["deployTargetConfigs"]:
-                            if existing_config['id'] in existing_config_ids and existing_config['branches'] not in specified_branch_patterns:
-                                self._display.vvvv(f"Deleting unmatched config with ID {existing_config['id']}")
-                                if DeployTargetConfig(self.client).delete(project['id'], existing_config['id']):
-                                    result['result'].append({'id': existing_config['id'], 'deleted': True})
-                                    result['changed'] = True
+                    result['changed'] = True
+
 
             elif state == "absent":
                 result['changed'] = False
