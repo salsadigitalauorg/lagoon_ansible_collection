2dcf45d6f62bd2c004cd3201b0d15163b167f2e1
rashed-k <rashedkhan.e36@gmail.com>
2024-03-04 13:59:19 +1100

    removed debuggin lines

--- api/plugins/action/metadata.py
+++ api/plugins/action/metadata.py
@@ -21,46 +21,35 @@ class ActionModule(LagoonActionBase):
         project_id = int(self._task.args.get('project_id', None))
         project_name = self._task.args.get('project_name', None)
 
-        print(f"rmk-debug: State={state}, Project ID={project_id}, Project Name={project_name}")
-
         result = {'result': [], 'invalid': [], 'changed': False, 'failed': False}
 
         if not isinstance(data, (dict, list)):
             result['failed'] = True
             result['message'] = 'Invalid data type; expected Dict or List'
-            print("rmk-debug: Invalid data type provided.")
             return result
 
         try:
             if project_name:
                 project_instance = Project(self.client).byName(project_name, ['metadata'])
-                print(f"rmk-debug: project_instance details: {project_instance}")
                 current_metadata = project_instance.projects[0]['metadata'] if project_instance.projects else {}
-                print(f"rmk-debug: current_metadata: {current_metadata}")
-                print(f"rmk-debug: Fetched current metadata for project '{project_name}'.")
             else:
                 current_metadata = {}  # Assuming no project name means no current metadata
-                print("rmk-debug: No project name provided, proceeding without current metadata.")
         except Exception as e:
             result['failed'] = True
             result['message'] = f"Error fetching project metadata: {e}"
-            print(f"rmk-debug: Exception caught while fetching project metadata: {e}")
             return result
 
         lagoonMetadata = Metadata(self.client)
 
         def is_change_required(key, value):
             required = current_metadata.get(key) != value
-            print(f"rmk-debug: Change required for '{key}'? {required}")
             return required
 
         if state == 'present':
-            print("rmk-debug: Processing state 'present'.")
             if isinstance(data, list):
                 for item in data:
                     if not isinstance(item, dict) or 'key' not in item or 'value' not in item:
                         result['invalid'].append(item)
-                        print(f"rmk-debug: Invalid item skipped: {item}")
                         continue
                     key, value = item['key'], item['value']
                     if is_change_required(key, value):
@@ -68,10 +57,8 @@ class ActionModule(LagoonActionBase):
                             update_result = lagoonMetadata.update(project_id, key, value)
                             result['result'].append({key: value})
                             result['changed'] = True
-                            print(f"rmk-debug: Updated metadata list for key '{key}' with value '{value}'.")
                         except Exception as e:
                             result['invalid'].append(key)
-                            print(f"rmk-debug: Exception caught list while updating '{key}': {e}")
             else:  # if data is a dict
                 for key, value in data.items():
                     if is_change_required(key, value):
@@ -79,13 +66,10 @@ class ActionModule(LagoonActionBase):
                             update_result = lagoonMetadata.update(project_id, key, value)
                             result['result'].append({key: value})
                             result['changed'] = True
-                            print(f"rmk-debug: Updated metadata for key '{key}' with value '{value}'.")
                         except Exception as e:
                             result['invalid'].append(key)
-                            print(f"rmk-debug: Exception caught while updating '{key}': {e}")
 
         elif state == 'absent':
-            print("rmk-debug: Processing state 'absent'.")
             # handle both list and dictionaries
             if isinstance(data, list):
                 keys_to_remove = []
@@ -97,7 +81,6 @@ class ActionModule(LagoonActionBase):
             else:
                 keys_to_remove = list(data.keys())
 
-            print("rmk-debug: Keys to remove -- ", keys_to_remove)
 
             for key in keys_to_remove:
                 if key in current_metadata:
@@ -105,18 +88,11 @@ class ActionModule(LagoonActionBase):
                         remove_result = lagoonMetadata.remove(project_id, key)
                         result['result'].append({key: 'removed'})
                         result['changed'] = True
-                        print(f"rmk-debug: Removed metadata for key '{key}'.")
                     except Exception as e:
                         result['invalid'].append(key)
-                        print(f"rmk-debug: Exception caught while removing '{key}': {e}")
-
-
-
 
         if result['invalid']:
             result['failed'] = True
             result['message'] = f"Errors occurred with keys: {', '.join(result['invalid'])}"
-            print(f"rmk-debug: Operation completed with errors: {result['invalid']}")
 
-        print(f"rmk-debug: Final result: {result}")
         return result
