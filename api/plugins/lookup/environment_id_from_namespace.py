e7e1486f22aea3bb90ac3be2852364d8083e82a4
Yusuf A. Hasan Miyan <yusufh@protonmail.com>
2024-09-23 16:45:28 +0400

    feat: added environment_id_from_namespace lookup plugin

---
+++ api/plugins/lookup/environment_id_from_namespace.py
@@ -0,0 +1,69 @@
+from . import LagoonLookupBase
+from ansible.errors import AnsibleError
+from gql.dsl import DSLQuery
+
+DOCUMENTATION = """
+  name: environment_id_from_namespace
+  author: Yusuf Hasan Miyan <yusuf@salsa.digital>
+  short_description: Get a lagoon environment's id from its namespace.
+  description:
+    - This lookup returns the id of an environment in Lagoon from its k8s namespace.
+  options:
+    _terms:
+      description: The namespace.
+      required: True
+    fail_on_not_found:
+      description: Flag to control whether to fail if the environment is not found.
+      type: boolean
+      default: False
+    lagoon_api_endpoint:
+      description: The Lagoon graphql endpoint
+      type: string
+      required: True
+      vars:
+        - name: lagoon_api_endpoint
+    lagoon_api_token:
+      description: The token for Lagoon graphql API
+      type: string
+      required: True
+      vars:
+        - name: lagoon_api_token
+    headers:
+      description: HTTP request headers
+      type: dictionary
+      default: {}
+"""
+
+EXAMPLES = """
+- name: retrieve an environment id
+  debug: msg="{{ lookup('lagoon.api.environment_id_from_namespace', environment_ns) }}"
+"""
+
+
+class LookupModule(LagoonLookupBase):
+
+  def run(self, terms, variables=None, **kwargs):
+
+    ret = []
+
+    self.set_options(var_options=variables, direct=kwargs)
+
+    self.createClient()
+
+    for term in terms:
+      with self.client:
+        qry = self.client.build_dynamic_query(
+          'environmentByKubernetesNamespaceName',
+          'Environment',
+          {'kubernetesNamespaceName': term},
+          ['id'])
+        result = self.client.execute_query_dynamic(DSLQuery(qry))
+        if 'environmentByKubernetesNamespaceName' not in result:
+          raise AnsibleError(f"Error finding environment '{term}': {result}")
+
+        if result['environmentByKubernetesNamespaceName'] == None and self.get_option('fail_on_not_found', False):
+          raise AnsibleError(f"Environment '{term}' not found")
+        elif isinstance(result['environmentByKubernetesNamespaceName'], dict):
+          ret.append(result['environmentByKubernetesNamespaceName']['id'])
+
+    return ret
